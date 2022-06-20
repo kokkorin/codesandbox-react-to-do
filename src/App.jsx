3bcb4a59ffa4c21debfaf6135a6772159c1944b5
kokkorin <104617188+kokkorin@users.noreply.github.com>
2022-06-20 15:50:51 +0900

    clssNameを追加

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,31 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了リストのTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了リストのTODO</p>
         <ul>
-          <li>
-            <div>
-              <p>あああああ</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
+          <li className="list-row">
+            <p>あああああ</p>
+            <button>完了</button>
+            <button>削除</button>
           </li>
-          <li>
-            <div>
-              <p>あああああ</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
+          <li className="list-row">
+            <p>あああああ</p>
+            <button>完了</button>
+            <button>削除</button>
           </li>
         </ul>
-        <p>完了リストのTODO</p>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了リストのTODO</p>
         <ul>
-          <li>
-            <div>
-              <p>ううううう</p>
-              <button>戻す</button>
-            </div>
-          </li>
-          <li>
-            <div>
-              <p>えええええ</p>
-              <button>戻す</button>
-            </div>
+          <li className="list-row">
+            <p>ううううう</p>
+            <button>戻す</button>
           </li>
         </ul>
       </div>
